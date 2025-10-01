6f4a26331ace3a25eb778dda56166ce59dfb5787
Raul <r.palaolozano@edu.gva.es>
2025-10-01 17:36:25 +0200

    Subo todo el proyecto

---
+++ src/AdiosRaul.java
@@ -0,0 +1,5 @@
+public class AdiosRaul {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Adios, Raúl!");
+    }
+}
---
+++ src/paco/HolaPaco.java
@@ -0,0 +1,7 @@
+package paco;
+
+public class HolaPaco {
+    public static void main(String[] args) throws Exception {
+        System.out.println();
+    }
+}
