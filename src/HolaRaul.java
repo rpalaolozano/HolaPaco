fc80872923496f7730022fa5752d9f1c4fa50074
Raul <r.palaolozano@edu.gva.es>
2025-10-01 17:52:32 +0200

    He modificado el fichero HolaRaul

--- src/HolaRaul.java
+++ src/HolaRaul.java
@@ -1,5 +1,5 @@
 public class HolaRaul {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Raúl!");
+        System.out.println("Hola, Raúl!");
     }
 }
