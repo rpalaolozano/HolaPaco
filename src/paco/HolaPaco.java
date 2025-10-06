8f57641dbc55b4a5e12a22292548ecd4776c151b
Raul <r.palaolozano@edu.gva.es>
2025-10-06 11:32:46 +0200

    Cambio a Juan

--- src/paco/HolaPaco.java
+++ src/paco/HolaPaco.java
@@ -2,6 +2,6 @@ package paco;
 
 public class HolaPaco {
     public static void main(String[] args) throws Exception {
-        System.out.println();
+        System.out.println("Hola Juan");
     }
 }
