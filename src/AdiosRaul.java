7b9c829d1f622d30d436ddd93133ffd576aa4427
Raul <r.palaolozano@edu.gva.es>
2025-10-06 11:57:19 +0200

    Pongo adios roberto

--- src/AdiosRaul.java
+++ src/AdiosRaul.java
@@ -1,5 +1,5 @@
 public class AdiosRaul {
     public static void main(String[] args) throws Exception {
-        System.out.println("Adios Raul!");
+        System.out.println("Adios Roberto!");
     }
 }
